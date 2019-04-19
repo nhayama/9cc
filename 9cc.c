98d3b207b37149823a4c9e92e6a1244bf7031ec2
Naoki Hayama <naoki.hayama0630@gmail.com>
2019-04-19 23:11:50 +0900

    introdeced a tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,61 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define TOKEN_MAX (100)
+
+// values representing the types of tokens
+enum {
+  TK_NUM = 256, // digit token
+  TK_EOF,       // EOF token
+};
+
+typedef struct {
+  int ty;      // type
+  int val;     // value (when ty is TK_NUM)
+  char *input; // token string (for an error message)
+} Token;
+
+Token tokens[TOKEN_MAX];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "cannot tokenize: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "unexpected token: %s\n",
+	  tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -7,28 +63,37 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char *p = argv[1];
+  tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov eax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add eax, %ld\n", strtol(p, &p, 10));
+  if (tokens[0].ty != TK_NUM)
+    error(0);
+  printf("  mov eax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+	error(i);
+      printf("  add eax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub eax, %ld\n", strtol(p, &p, 10));
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+	error(i);
+      printf("  sub eax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    fprintf(stderr, "unexpected char: '%c'\n", *p);
-    return 1;
+    error(i);
   }
 
   printf("  ret\n");
